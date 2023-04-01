3fcf404706347c3ab11e35e274f7514f62610b2d
Pedro Bonett <gilbonett@gmail.com>
2023-04-01 16:52:59 -0300

    configuration initial project adopet

--- app/server.js
+++ app/server.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = process.env.port || 3000;
+const { sequelize } = require("./models/index.js");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(require('./routes'))
+app.use(require("./routes"));
+
 app.listen(port, () => {
-  console.log(`Connecting ${port}`)
-})
+  console.log(`Connecting ${port}`);
+
+  sequelize.authenticate().then(() => {console.log('Connect Succesfull')})
+});
---
+++ config/database.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+module.exports= { 
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "110910",
+  database: process.env.DB_DATABASE || "database_development",
+  host: process.env.DB_HOST || "127.0.0.1",
+  dialect: process.env.DB_DIALECT || "mysql",
+
+
+  seederStorage: "sequelize",
+  seederStorageTableName: "seeds",
+
+  migrationStorage: "sequelize",
+  migrationStorageTableName: "migrations",
+}
