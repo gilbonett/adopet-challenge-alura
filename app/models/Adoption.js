01767cbbb9752053f6366e3c83f6c6e63c7af75b
Pedro Bonett <gilbonett@gmail.com>
2023-04-27 10:40:13 -0300

    Restructuring adoption models

--- app/models/Adoption.js
+++ app/models/Adoption.js
@@ -4,29 +4,47 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Adoption extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+  
     static associate(models) {
-      Adoption.belongsTo(models.Tutor, {
+      Adoption.belongsTo(models.User, { 
         foreignKey:'tutor_id'
       })
       Adoption.belongsTo(models.Pet, {
         foreignKey:'pet_id'
       })
-      Adoption.belongsTo(models.Shelter, {
-        foreignKey:'shelter_id'
-      })
     }
-    
   }
   Adoption.init({
+    UUID: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+      unique: true
+    },
+    tutor_id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {       
+        model: 'User',
+        key: 'id',
+        role: 'tutor'
+      }
+    },
+    pet_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
   }, {
     sequelize,
+    paranoid: true,
     modelName: 'Adoption',
-    freezeTableName: true
+    freezeTableName: true,
+    timestamps: false,
+    primaryKey: false
   });
   return Adoption;
 };
